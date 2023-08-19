8d2543beec4f6224540dc3a2dbda3611d7dd15d3
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-08-19 16:26:12 +0300

    init session module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PassCollectionModule } from './pass-collection/pass-collection.module';
+import { SessionService } from './session/session.service';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [AuthModule, PassCollectionModule],
+  imports: [AuthModule, PassCollectionModule, SessionModule],
   exports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SessionService],
 })
 export class AppModule {}
