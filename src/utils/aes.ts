6a6b6067540e5da2e7c91418d6d50069fb98d9f3
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-08-26 12:46:19 +0300

    added eas

---
+++ src/utils/aes.ts
@@ -0,0 +1,35 @@
+import * as crypto from 'crypto';
+
+export class AES {
+  private aesKey: Buffer;
+  private iv: Buffer;
+
+  constructor() {
+    this.aesKey = crypto.randomBytes(32);
+    this.iv = crypto.randomBytes(16);
+  }
+
+  public generateKeys() {
+    this.aesKey = crypto.randomBytes(32);
+    this.iv = crypto.randomBytes(16);
+  }
+
+  public setKeys(key: Buffer, iv: Buffer) {
+    this.aesKey = key;
+    this.iv = iv;
+  }
+
+  public encrypt(message: string) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', this.aesKey, this.iv);
+    let encryptedMessage = cipher.update(message, 'utf-8', 'base64');
+    encryptedMessage += cipher.final('base64');
+    return encryptedMessage;
+  }
+
+  public decrypt(encryptedMessage: string) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', this.aesKey, this.iv);
+    let decryptedMessage = decipher.update(encryptedMessage, 'base64', 'utf-8');
+    decryptedMessage += decipher.final('utf-8');
+    return decryptedMessage;
+  }
+}
